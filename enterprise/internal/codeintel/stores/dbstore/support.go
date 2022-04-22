223aac4d81180cd7f2a26718a915c088d8dce99c
Eric Fritz <eric@eric-fritz.com>
2022-04-22 10:22:32 -0500

    Add dbstore implementation.

--- enterprise/internal/codeintel/stores/dbstore/support.go
+++ enterprise/internal/codeintel/stores/dbstore/support.go
@@ -3,15 +3,30 @@ package dbstore
 import (
 	"context"
 
-	"github.com/sourcegraph/sourcegraph/lib/errors"
+	"github.com/keegancsmith/sqlf"
+
+	"github.com/sourcegraph/sourcegraph/internal/database/basestore"
 )
 
 func (s *Store) RequestLanguageSupport(ctx context.Context, userID int, language string) error {
-	// TODO - implement
-	return errors.New("unimplemented - dbstore layer")
+	return s.Exec(ctx, sqlf.Sprintf(requestLanguageSupportQuery, userID, language))
 }
 
+const requestLanguageSupportQuery = `
+-- source: enterprise/internal/codeintel/stores/dbstore/support.go:RequestLanguageSupport
+INSERT INTO codeintel_langugage_support_requests (user_id, language_id)
+VALUES (%s, %s)
+ON CONFLICT DO NOTHING
+`
+
 func (s *Store) LanguagesRequestedBy(ctx context.Context, userID int) ([]string, error) {
-	// TODO - implement
-	return nil, errors.New("unimplemented - dbstore layer")
+	return basestore.ScanStrings(s.Query(ctx, sqlf.Sprintf(languagesRequestedByQuery, userID)))
 }
+
+const languagesRequestedByQuery = `
+-- source: enterprise/internal/codeintel/stores/dbstore/support.go:LanguagesRequestedBy
+SELECT language_id
+FROM codeintel_langugage_support_requests
+WHERE user_id = %s
+ORDER BY language_id
+`
